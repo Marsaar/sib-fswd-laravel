29aceee5b1aade2176a2f435d3dbaf8f2e7b3a17
Marsaar <marforpublic1@gmail.com>
2023-06-18 04:24:15 +0700

    API Product

---
+++ app/Http/Controllers/API/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $product = Product::all();
+        
+        return response()->json([
+            'success' => true,
+            'message' => 'daftar data produk',
+            'data' => $product,
+        ], 200);
+    }
+}
